d23c7db73a177175e0c76c261008f43a3545e045
叶灵雨 <970106@nd.com>
2020-08-26 21:55:04 +0800

    1.连接数据库配置

--- src/test/java/com/evan/coolrabbit/CoolrabbitApplicationTests.java
+++ src/test/java/com/evan/coolrabbit/CoolrabbitApplicationTests.java
@@ -1,12 +1,10 @@
 package com.evan.coolrabbit;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoolrabbitApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
