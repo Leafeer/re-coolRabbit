1eaee04e6b239ad7c07c4fe9bc51d2a407e87ab7
leafeer <leafeer@163.com>
2020-08-30 14:50:15 +0800

    1。配置数据库。2.配置数据库和前端联调

--- src/main/java/com/evan/coolrabbit/controller/LoginController.java
+++ src/main/java/com/evan/coolrabbit/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.evan.coolrabbit.controller;
 
 import com.evan.coolrabbit.pojo.User;
 import com.evan.coolrabbit.result.Result;
+import com.evan.coolrabbit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +16,17 @@ import java.util.Objects;
 @Controller
 public class LoginController {
 
+    @Autowired
+    UserService userService;
+
     @CrossOrigin
     @PostMapping(value = "api/login")
     @ResponseBody
     public Result logini(@RequestBody User requestUser) {
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
-        if(!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
+        User user = userService.getNameAndPassword(username, requestUser.getPassword());
+        if(null == user) {
             String message = "账号密码错误";
             System.out.println("test");
             return new Result(400);
